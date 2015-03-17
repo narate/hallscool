7c0c71e8a654a970bc144a2aa010808bc8c8d7bd
Narate Ketram <koonnarate@gmail.com>
2015-03-17 16:45:59 +0700

    add id variable and change url to string format

--- post.go
+++ post.go
@@ -3,48 +3,49 @@ package main
 import ("net/http"
         "log"
         "net/url"
-	"fmt"
+        "fmt"
         "math/rand"
-	"strconv"
+        "strconv"
         )
 
 func main()  {
 
-	// Edit Post URL by view source from google form
-        postUrl := "https://docs.google.com/forms/?/??????????????????????????????????/formResponse"
-	// Edit Max your post
-	Max := 10
+    // Edit Post URL by view source from google form
+    id := "????????????????????????????????????"
+    postUrl := fmt.Sprintf("https://docs.google.com/forms/d/%s/formResponse", id)
+    // Edit Max your post
+    Max := 10
 
-        values := make(url.Values)
+    values := make(url.Values)
 
-	for i:=0; i< Max ; i++{
+    for i:=0; i< Max ; i++{
 
-		//add and Edit your entry.xxxx it is name of google form
+        //add and Edit your entry.xxxx it is name of google form
 
-		//input text use randomString
-	        values.Set("entry.1352624538", randomString(10))
-	        values.Set("entry.668341357", randomString(10))
-		//input digit like Tel No. use strconv.Itoa(rand.Intn(10000000000))
-		values.Set("entry.1007776359",strconv.Itoa(rand.Intn(10000000000)))
-	        // Submit form
-	        resp, err := http.PostForm(postUrl, values)
-	        if err != nil {
-	                log.Fatal(err)
-	        }
-		fmt.Printf("[%d] %s\n",i,resp.Status)
-	        defer resp.Body.Close()
-	}
+        //input text use randomString
+        values.Set("entry.1352624538", randomString(10))
+        values.Set("entry.668341357", randomString(10))
+        //input digit like Tel No. use strconv.Itoa(rand.Intn(10000000000))
+        values.Set("entry.1007776359",strconv.Itoa(rand.Intn(10000000000)))
+        // Submit form
+        resp, err := http.PostForm(postUrl, values)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Printf("[%d] %s\n",i,resp.Status)
+        defer resp.Body.Close()
+    }
 
 }
 
 func randomString(l int) string{
-	var char string
-	buf := make([]byte, l)
-	char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        for i:=0 ; i<l ; i++ {
-		buf[i] = char[rand.Intn(len(char)-1)]
-	}
-	return string(buf)
+    var char string
+    buf := make([]byte, l)
+    char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    for i:=0 ; i<l ; i++ {
+        buf[i] = char[rand.Intn(len(char)-1)]
+    }
+    return string(buf)
 }
 
 func randInt(min int, max int) int {
